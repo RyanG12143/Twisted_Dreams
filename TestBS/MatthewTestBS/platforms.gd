6d48fb9166fbad1f88b87dd978b98444b3983292
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-05-30 11:22:27 -0500

    More trial and error

--- TestBS/MatthewTestBS/platforms.gd
+++ TestBS/MatthewTestBS/platforms.gd
@@ -4,18 +4,21 @@ extends Node2D
 @export var distance: Vector2 = Vector2(0, -100)
 @export var duration: float = 5.0
 var start_pos: Vector2
+
 signal object_on
 signal object_off
 
 func start_movement():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	tween.tween_property($Platform1Body, "position", distance, duration / 2)
+	tween.tween_property($Platform1Body, "position", -(distance), duration / 2)
+	print("Going down")
 
 func backwards_movement():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	tween.tween_property($Platform1Body, "position", -(distance), duration / 2)
+	tween.tween_property($Platform1Body, "position", distance, duration / 2)
+	print("Going up")
 
 func _on_area_2d_body_entered(body):
 	start_movement()
@@ -24,3 +27,6 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	backwards_movement()
 	object_off.emit()
+
+func _on_pulley_platform_object_on():
+	backwards_movement()
