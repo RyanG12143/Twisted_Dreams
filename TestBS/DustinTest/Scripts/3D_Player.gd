f2bd7989661a63b3470d8f745f25baf3b50e6665
Rookioo <andersond5235@my.uwstout.edu>
2024-05-31 15:30:29 -0500

    Changing to Control Nodes

--- TestBS/DustinTest/Scripts/3D_Player.gd
+++ TestBS/DustinTest/Scripts/3D_Player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED:float = 5.0
+const JUMP_VELOCITY:float = 4.5
 @onready var pivot = $CamOrigin
-@export var sens = 0.5
+@export var sens:float = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
--- TestBS/DustinTest/Scripts/Level_Select.gd
+++ TestBS/DustinTest/Scripts/Level_Select.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 func _on_first_level_pressed():
 	get_tree().change_scene_to_file("res://TestBS/LeoPuzzlePrototype/world.tscn")
