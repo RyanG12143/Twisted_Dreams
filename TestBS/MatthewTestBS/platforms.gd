4528f617d8e70a0258d7a81657cd84dd2f53f2e1
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-06-03 13:03:09 -0500

    Continued to work on pulley platforms

--- TestBS/MatthewTestBS/platforms.gd
+++ TestBS/MatthewTestBS/platforms.gd
@@ -1,30 +1,39 @@
-class_name Pulley
 extends Sprite2D
 
-@export var distance: float = 20.0
+@export var distance: float = 50.0
 var start_pos: Vector2
+var objects: int = 0
+var t: float = 0.0
 
 signal object_on
 signal object_off
 
-func start_movement():
-	position = position.lerp(Vector2(position.x, position.y + distance), 1)
-	print("Going down")
+func _ready():
+	start_pos = position
 
-func backwards_movement():
-	
-	print("Going up")
+func _process(delta):
+	t += delta * 0.4
+	if objects > 0:
+		position = position.lerp(Vector2(position.x, start_pos.y + distance), t)
+	if objects < 0:
+		position = position.lerp(Vector2(position.x, start_pos.y - distance), t)
+	if objects == 0 and position != start_pos:
+		position = position.lerp(Vector2(position.x, start_pos.y), t)
+
+#func start_movement():
+	#pass
+#
+#func backwards_movement():
+	#
+	#print("Going up")
 
 func _on_area_2d_body_entered(body):
-	start_movement()
+	objects += 1
+	print("Going down")
 	object_on.emit()
 
+
 func _on_area_2d_body_exited(body):
-	backwards_movement()
+	objects -= 1
+	print("Going up")
 	object_off.emit()
-
-func _on_pulley_platform_object_on():
-	backwards_movement()
-
-func _on_pulley_platform_object_off():
-	start_movement()
