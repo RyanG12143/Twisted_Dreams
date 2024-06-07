e3eb893a356627300fb7da8463547e98d41ed9f4
Rookioo <andersond5235@my.uwstout.edu>
2024-06-06 20:39:37 -0500

    Added Sprinting

--- TestBS/DustinTest/Scripts/3D_Player_Older.gd
+++ TestBS/DustinTest/Scripts/3D_Player_Older.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED:float = 5.0
-const JUMP_VELOCITY:float = 4.5
+var current_speed:float
+@export var walk_speed:float = 4.0
+@export var sprint_speed:float = 6.0
+var stored_speed:float
+@export var jump_velocity:float = 4.5
 @onready var pivot = $CamOrigin
 @export var sens:float = 0.5
 var called_climb = false
@@ -38,7 +41,12 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("3Djump") and is_on_floor() and can_jump:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
+	
+	if Input.is_action_pressed("3Dsprint"):
+		current_speed = sprint_speed
+	else:
+		current_speed = walk_speed
 
 	if Input.is_action_just_pressed("escape"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -49,11 +57,11 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("3Dleft", "3Dright", "3Dforward", "3Dbackward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
 	
