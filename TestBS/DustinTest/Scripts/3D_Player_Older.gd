12553c44d745e5e144acc89396e1af7a69ac5866
Rookioo <andersond5235@my.uwstout.edu>
2024-06-10 21:26:27 -0500

    Broken 3D camera/New Camera Addon

--- TestBS/DustinTest/Scripts/3D_Player_Older.gd
+++ TestBS/DustinTest/Scripts/3D_Player_Older.gd
@@ -17,22 +17,76 @@ var grab_start = false
 @onready var chest_ray = $Rays/Chest
 @onready var head_rays = $Rays/HeadRays
 
-#climbing
-#var vertical_movement
-#var forward_movement
+#camera stuff
+@onready var _player_pcam: PhantomCamera3D
+@onready var _player_direction: Node3D = %PlayerDirection
+
+@export var mouse_sensitivity: float = 0.05
+
+@export var min_pitch: float = -89.9
+@export var max_pitch: float = 50
+
+@export var min_yaw: float = 0
+@export var max_yaw: float = 360
+
+
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+	_player_pcam = get_node("../PhantomCamera3D")
+
+	if _player_pcam.get_follow_mode() == _player_pcam.FollowMode.THIRD_PERSON:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * sens))
-		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
-		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+		pass
+		#rotate_y(deg_to_rad(-event.relative.x * sens))
+		#pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
+		#pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+
+#camera stuff
+
+func _unhandled_input(event: InputEvent) -> void:
+	if _player_pcam.get_follow_mode() == _player_pcam.FollowMode.THIRD_PERSON:
+		var active_pcam: PhantomCamera3D
+
+		if is_instance_valid(_player_pcam):
+			_set_pcam_rotation(_player_pcam, event)
+
+
+func _set_pcam_rotation(pcam: PhantomCamera3D, event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var pcam_rotation_degrees: Vector3
+
+		# Assigns the current 3D rotation of the SpringArm3D node - so it starts off where it is in the editor
+		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
+
+		# Change the X rotation
+		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
+
+		# Clamp the rotation in the X axis so it go over or under the target
+		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_pitch, max_pitch)
+
+		# Change the Y rotation value
+		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
+
+		# Sets the rotation to fully loop around its target, but witout going below or exceeding 0 and 360 degrees respectively
+		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_yaw, max_yaw)
+
+		# Change the SpringArm3D node's rotation and rotate around its target
+		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
+
+
+
+
+
+
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -62,7 +116,11 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
-
+	
+	if velocity.length() > 0.2:
+		var look_direction: Vector2 = Vector2(velocity.z, velocity.x)
+		_player_direction.rotation.y = look_direction.angle()
+	
 	move_and_slide()
 	
 func _process(delta):
