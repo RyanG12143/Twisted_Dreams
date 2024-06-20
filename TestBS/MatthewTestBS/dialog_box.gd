59ef60ab73e7697056194df7a05e6bfd5107306c
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-06-20 11:56:46 -0500

    Updated dialog box

--- TestBS/MatthewTestBS/dialog_box.gd
+++ TestBS/MatthewTestBS/dialog_box.gd
@@ -1,19 +1,24 @@
-extends RichTextLabel
+extends Polygon2D
 
-var dialog = ["This is a dialog box, yeah?", "Damn that's crazy"]
+@export var dialog: Array[String] = []
 var page: int = 0
 
 func _ready():
-	text = dialog[page]
-	set_visible_characters(0)
+	$DialogText.text = dialog[page]
+	$DialogText.set_visible_characters(0)
 	set_process_input(true)
 
 func _input(event):
-	if event == InputEventMouseButton and event.is_pressed():
-		if get_visible_characters() > get_total_character_count():
-			if page < dialog.size - 1:
+	if InputEventMouseButton and event.is_pressed():
+		if $DialogText.get_visible_characters() > $DialogText.get_total_character_count():
+			if page < dialog.size() - 1:
 				page += 1
-				text = dialog[page]
+				$DialogText.text = dialog[page]
+				$DialogText.set_visible_characters(0)
+			elif page == dialog.size() - 1:
+				hide()
+		else:
+			$DialogText.set_visible_characters($DialogText.get_total_character_count())
 
 func _on_scrolling_timer_timeout():
-	set_visible_characters(get_visible_characters() + 1)
+	$DialogText.set_visible_characters($DialogText.get_visible_characters() + 1)
