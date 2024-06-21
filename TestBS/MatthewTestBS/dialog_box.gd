1d20092c7b7d8ff62d1895af6aa7bcbff8a66930
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-06-21 12:40:42 -0500

    Updated Dialog box scene

--- TestBS/MatthewTestBS/dialog_box.gd
+++ TestBS/MatthewTestBS/dialog_box.gd
@@ -1,21 +1,34 @@
-extends Polygon2D
+extends CanvasLayer
 
 @export var dialog: Array[String] = []
 var page: int = 0
 
 func _ready():
+	$AnimationPlayer.play("go_up")
 	$DialogText.text = dialog[page]
 	$DialogText.set_visible_characters(0)
+	$DialogBox/Character2.modulate = Color("gray")
 	set_process_input(true)
+	await get_tree().create_timer(0.5).timeout
+	
+	$ScrollingTimer.start()
 
 func _input(event):
 	if InputEventMouseButton and event.is_pressed():
 		if $DialogText.get_visible_characters() > $DialogText.get_total_character_count():
 			if page < dialog.size() - 1:
 				page += 1
+				if page % 2 != 0:
+					$DialogBox/Character1.modulate = Color("gray")
+					$DialogBox/Character2.modulate = Color("white")
+				else:
+					$DialogBox/Character2.modulate = Color("gray")
+					$DialogBox/Character1.modulate = Color("white")
 				$DialogText.text = dialog[page]
 				$DialogText.set_visible_characters(0)
 			elif page == dialog.size() - 1:
+				$AnimationPlayer.play("go_down")
+				await get_tree().create_timer(0.5).timeout
 				hide()
 		else:
 			$DialogText.set_visible_characters($DialogText.get_total_character_count())
