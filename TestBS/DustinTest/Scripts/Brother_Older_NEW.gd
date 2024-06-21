b1bf4ade35415e3993301e573440721e1cb48f1b
Rookioo <andersond5235@my.uwstout.edu>
2024-06-20 20:15:25 -0500

    Much Smoother 3D Movement

--- TestBS/DustinTest/Scripts/Brother_Older_NEW.gd
+++ TestBS/DustinTest/Scripts/Brother_Older_NEW.gd
@@ -17,6 +17,7 @@ extends CharacterBody3D
 
 @export var min_yaw: float = 0
 @export var max_yaw: float = 360
+@onready var mesh = %PlayerVisual
 
 # Climbing vars
 var called_climb = false
@@ -25,10 +26,10 @@ var can_jump = true
 var grab_start = false
 var can_turn: bool = true
 
-@export var WALK_SPEED: float = 2.5
-@export var SPRINT_SPEED: float = 4.0
-var CURRENT_SPEED: float = WALK_SPEED
-@export var JUMP_VELOCITY: float = 3.0
+@export var walk_speed: float = 2.5
+@export var run_speed: float = 4.0
+@export var movement_speed: float
+@export var JUMP_VELOCITY: float = 10.0
 @export var enable_gravity = true
 
 @onready var _camera: Camera3D
@@ -40,54 +41,16 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var movement_enabled: bool = true
 
-var _physics_body_trans_last: Transform3D
-var _physics_body_trans_current: Transform3D
-
-const KEY_STRINGNAME: StringName = "Key"
-const ACTION_STRINGNAME: StringName = "Action"
-
-const INPUT_MOVE_UP_STRINGNAME: StringName = "move_up"
-const INPUT_MOVE_DOWM_STRINGNAME: StringName = "move_down"
-const INPUT_MOVE_LEFT_STRINGNAME: StringName = "move_left"
-const INPUT_MOVE_RIGHT_STRINGNAME: StringName = "move_right"
-
-# Making dictionary for all inputs
-var InputMovementDic: Dictionary = {
-	INPUT_MOVE_UP_STRINGNAME: {
-		KEY_STRINGNAME: KEY_W,
-		ACTION_STRINGNAME: INPUT_MOVE_UP_STRINGNAME
-	},
-	INPUT_MOVE_DOWM_STRINGNAME: {
-		KEY_STRINGNAME: KEY_S,
-		ACTION_STRINGNAME: INPUT_MOVE_DOWM_STRINGNAME
-	},
-	INPUT_MOVE_LEFT_STRINGNAME: {
-		KEY_STRINGNAME: KEY_A,
-		ACTION_STRINGNAME: INPUT_MOVE_LEFT_STRINGNAME
-	},
-	INPUT_MOVE_RIGHT_STRINGNAME: {
-		KEY_STRINGNAME: KEY_D,
-		ACTION_STRINGNAME: INPUT_MOVE_RIGHT_STRINGNAME
-	},
-}
-
-
+# New movement vars
+var direction: Vector3 = Vector3.FORWARD
+var velocity_move: Vector3 = Vector3.ZERO
+var acceleration = 5
 
 # Ready function
 func _ready():
 	# Setting up key values and action values for player input
-	for input in InputMovementDic:
-		var key_val = InputMovementDic[input].get(KEY_STRINGNAME)
-		var action_val = InputMovementDic[input].get(ACTION_STRINGNAME)
-
-		_camera = owner.get_node("%MainCamera3D")
-
-		var movement_input = InputEventKey.new()
-		movement_input.physical_keycode = key_val
-		InputMap.add_action(action_val)
-		InputMap.action_add_event(action_val, movement_input)
-
-		_player_visual.top_level = true
+	_camera = owner.get_node("%MainCamera3D")
+	_player_visual.top_level = true
 	
 	_player_pcam = get_node("../PhantomCamera3D")
 
@@ -126,53 +89,18 @@ func _set_pcam_rotation(pcam: PhantomCamera3D, event: InputEvent) -> void:
 
 # Physics process function
 func _physics_process(delta):
-	_physics_body_trans_last = _physics_body_trans_current
-	_physics_body_trans_current = global_transform
-
+	# If movement isn't enabled cancel all movement
+	if not movement_enabled: return
+	
 	# Add the gravity.
 	if enable_gravity and not is_on_floor():
 		velocity.y -= gravity * delta
-	
-	# If movement isn't enabled cancel all movement
-	if not movement_enabled: return
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir: Vector2 = Input.get_vector(
-		INPUT_MOVE_LEFT_STRINGNAME,
-		INPUT_MOVE_RIGHT_STRINGNAME,
-		INPUT_MOVE_UP_STRINGNAME,
-		INPUT_MOVE_DOWM_STRINGNAME
-	)
-
-	var cam_dir: Vector3 = -_camera.global_transform.basis.z
-	
-	# Changing between walking and sprinting
-	if Input.is_action_pressed("3Dsprint"):
-		CURRENT_SPEED = SPRINT_SPEED
-	else:
-		CURRENT_SPEED = WALK_SPEED
+		
+	var cam_dir: Vector3 = _camera.global_transform.basis.z
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("3Djump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Getting input direction and moving
-	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		var move_dir: Vector3 = Vector3.ZERO
-		move_dir.x = direction.x
-		move_dir.z = direction.z
-
-		move_dir = move_dir.rotated(Vector3.UP, _camera.rotation.y).normalized()
-		if(is_on_floor()):
-			velocity.x = move_dir.x * CURRENT_SPEED
-			velocity.z = move_dir.z * CURRENT_SPEED
-
-	else:
-		if(is_on_floor()):
-			velocity.x = move_toward(velocity.x, 0, CURRENT_SPEED)
-			velocity.z = move_toward(velocity.z, 0, CURRENT_SPEED)
 	
 	# Quitting to menu
 	if Input.is_action_just_pressed("escape"):
@@ -182,24 +110,46 @@ func _physics_process(delta):
 	# setting the look direction
 	var testVelocity: Vector3 = Vector3(velocity.x, 0, velocity.z)
 	if testVelocity.length() > 0.2 and !chest_ray.is_colliding() and can_turn:
-		var look_direction: Vector2 = Vector2(velocity.z, velocity.x)
+		var look_direction: Vector2 = -Vector2(velocity.z, velocity.x)
 		_player_direction.rotation.y = look_direction.angle()
-	move_and_slide()
 	
+	direction = Vector3(Input.get_action_strength("3Dleft") - Input.get_action_strength("3Dright"),
+		0,
+		Input.get_action_strength("3Dforward") - Input.get_action_strength("3Dbackward"))
+	
+	if Input.is_action_pressed("3Dforward") || Input.is_action_pressed("3Dbackward") || Input.is_action_pressed("3Dleft") || Input.is_action_pressed("3Dright"):
+		if Input.is_action_pressed("3Dsprint"):
+			movement_speed = run_speed
+		else:
+			movement_speed = walk_speed
+	else:
+		movement_speed = 0
+	
+	var move_dir = direction.rotated(Vector3.UP, _camera.rotation.y).normalized()
+	
+	velocity_move = lerp(velocity, -move_dir * movement_speed, delta * acceleration)
+	
+	velocity = velocity_move
+
+	move_and_slide()
 	
 
 # Process function
 func _process(delta):
-	#_player_visual.global_transform = _physics_body_trans_last.interpolate_with(
-		#_physics_body_trans_current,
-		#Engine.get_physics_interpolation_fraction()
-	#)
-	
 	if Input.is_action_pressed("3Djump") and can_climb():
 		grab_ledge()
-		
 	
-
+	var fps = Engine.get_frames_per_second()
+	var lerp_interval = velocity_move / fps
+	var lerp_position = global_transform.origin + lerp_interval
+	
+	if fps > 60:
+		mesh.top_level = true
+		mesh.global_transform.origin = mesh.global_transform.origin.lerp(lerp_position, 20 * delta)
+	else:
+		mesh.global_transform = global_transform
+		mesh.top_level = false
+	
 # Climbing functions
 func can_climb():
 	if !chest_ray.is_colliding():
@@ -261,7 +211,7 @@ func climb():
 	var v_move_time = 1
 	var h_move_time = 0.4
 	var vertical_movement = global_transform.origin + Vector3(0, 1.9, 0)
-	var forward_movement = global_transform.origin + (_player_direction.global_transform.basis.z * 1.2)
+	var forward_movement = global_transform.origin + (-_player_direction.global_transform.basis.z * 1.2)
 	forward_movement.y += 1.8
 	
 	var tween = create_tween()
