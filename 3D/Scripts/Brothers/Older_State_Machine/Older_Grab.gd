f51f331d2478a23e26c5ef775f84c46a2b347e65
Rookioo <andersond5235@my.uwstout.edu>
2024-06-22 17:43:21 -0500

    Prototype Brothers House
    Added Zooming

--- 3D/Scripts/Brothers/Older_State_Machine/Older_Grab.gd
+++ 3D/Scripts/Brothers/Older_State_Machine/Older_Grab.gd
@@ -4,10 +4,11 @@ class_name Older_Grab
 func Enter():
 	enable_gravity = false
 	older_brother.velocity = Vector3.ZERO
-	await get_tree().create_timer(0.5).timeout
-	Transitioned.emit(self, "Older_Hang")
 	# Facing wall normal
 	mesh.rotation.y = -(atan2(player_normal.get_collision_normal().z, player_normal.get_collision_normal().x) - PI/2)
+	await get_tree().create_timer(0.5).timeout
+	Transitioned.emit(self, "Older_Hang")
+	
 
 func Update(delta: float):
 	pass
