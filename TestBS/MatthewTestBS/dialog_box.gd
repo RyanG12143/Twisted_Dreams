4e284f3ebe4c8171fcdd6ef79deed6f29a1b364b
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-06-24 12:22:12 -0500

    Finished mock dialog box

--- TestBS/MatthewTestBS/dialog_box.gd
+++ TestBS/MatthewTestBS/dialog_box.gd
@@ -4,14 +4,7 @@ extends CanvasLayer
 var page: int = 0
 
 func _ready():
-	$AnimationPlayer.play("go_up")
-	$DialogText.text = dialog[page]
-	$DialogText.set_visible_characters(0)
-	$DialogBox/Character2.modulate = Color("gray")
-	set_process_input(true)
-	await get_tree().create_timer(0.5).timeout
-	
-	$ScrollingTimer.start()
+	hide()
 
 func _input(event):
 	if InputEventMouseButton and event.is_pressed():
@@ -30,8 +23,27 @@ func _input(event):
 				$AnimationPlayer.play("go_down")
 				await get_tree().create_timer(0.5).timeout
 				hide()
+				get_tree().paused = false
 		else:
 			$DialogText.set_visible_characters($DialogText.get_total_character_count())
 
 func _on_scrolling_timer_timeout():
 	$DialogText.set_visible_characters($DialogText.get_visible_characters() + 1)
+
+func start_dialog():
+	get_tree().paused = true
+	show()
+	$AnimationPlayer.play("go_up")
+	$DialogText.text = dialog[page]
+	$DialogText.set_visible_characters(0)
+	$DialogBox/Character2.modulate = Color("gray")
+	set_process_input(true)
+	await get_tree().create_timer(0.5).timeout
+	
+	$ScrollingTimer.start()
+
+func _on_area_2d_body_entered(body):
+	#get_tree().paused = true
+	print("Entered")
+	start_dialog()
+	
