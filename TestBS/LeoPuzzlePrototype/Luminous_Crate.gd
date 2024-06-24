80fec90b4a34f7a27e441c477a1b2a105b6acc27
RyanG12143 <loop9981@gmail.com>
2024-06-24 17:30:10 -0500

    Luminous crate done

--- TestBS/LeoPuzzlePrototype/Luminous_Crate.gd
+++ TestBS/LeoPuzzlePrototype/Luminous_Crate.gd
@@ -1,3 +1,23 @@
 extends Crate
 class_name Luminous_Cr
 ## Moveable, weighted luminous crates.
+
+
+var enemies:Array[CharacterBody2D] = []
+
+
+func _on_enemy_enter(body):
+	if body.is_in_group("Enemy"):
+		body.flee(self)
+		enemies.append(body)
+
+
+func _on_enemy_exit(body):
+	if body.is_in_group("Enemy"):
+		body.flee(self)
+		enemies.erase(body)
+
+
+func _on_timer_timeout():
+	for enemy in enemies:
+		enemy.flee(self)
