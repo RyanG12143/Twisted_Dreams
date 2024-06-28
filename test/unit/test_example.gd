21c5b02f5a737980773d053233464b6783c970c1
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-06-28 16:17:13 -0500

    Added gut and made a practice test

---
+++ test/unit/test_example.gd
@@ -0,0 +1,19 @@
+extends GutTest
+
+var Character = load("res://TestBS/LeoPuzzlePrototype/character.gd")
+var world_scene = load("res://TestBS/MatthewTestBS/world.tscn")
+var DoubledWorld = double(world_scene)
+
+func after_all():
+	queue_free()
+
+func test_passes():
+	#Will pass because 1 = 1
+	assert_eq(1,1)
+
+func test_jump():
+	var _character = Character.new()
+	var sender = InputSender.new(_character)
+	sender.action_down("ui_jump").wait_frames(1)
+	await(sender.idle)
+	assert_null(_character)
