50516cc8ef0f69f058f6b1a038225f5b856623b6
Rookioo <146274825+Rookioo@users.noreply.github.com>
2024-07-01 14:17:33 -0700

    Scene Manager

--- 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
+++ 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var transition = $Transition
-@onready var market_district = load("uid://dre0o1tw818oq")
+@export var scene_to_load: PackedScene
 
 func _on_area_3d_body_entered(body):
 	if body is CharacterBody3D:
@@ -9,4 +9,4 @@ func _on_area_3d_body_entered(body):
 
 func _on_transition_animation_finished(anim_name):
 	if(anim_name == "fade_out"):
-		get_tree().change_scene_to_packed(market_district)
+		get_tree().change_scene_to_packed(scene_to_load)
