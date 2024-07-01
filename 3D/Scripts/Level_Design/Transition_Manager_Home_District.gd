1eca20fb6fd04dce8a2077bf5654dc7001536a93
Rookioo <146274825+Rookioo@users.noreply.github.com>
2024-07-01 14:20:03 -0700

    Minor Changes Anim

--- 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
+++ 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
@@ -3,6 +3,9 @@ extends Node
 @onready var transition = $Transition
 @export var scene_to_load: PackedScene
 
+func ready():
+	transition.play("fade_in")
+
 func _on_area_3d_body_entered(body):
 	if body is CharacterBody3D:
 		transition.play("fade_out")
