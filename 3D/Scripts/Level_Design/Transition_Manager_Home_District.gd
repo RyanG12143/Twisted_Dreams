ea348bc27beabca1f8300d975a9bdbab2191168b
Rookioo <146274825+Rookioo@users.noreply.github.com>
2024-07-04 11:36:13 -0700

    Scene Loading Changes

--- 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
+++ 3D/Scripts/Level_Design/Transition_Manager_Home_District.gd
@@ -2,13 +2,14 @@ extends Node
 
 @onready var transition = $Transition
 @export var scene_to_load: PackedScene
+@export var transition_collider: Area3D
 
 func _ready():
 	await owner.ready
 	transition.play("fade_in")
 
-func _on_area_3d_body_entered(body):
-	if body is CharacterBody3D:
+func player_collision(transition_collider):
+	if transition_collider.body_entered is CharacterBody3D:
 		transition.play("fade_out")
 
 func _on_transition_animation_finished(anim_name):
