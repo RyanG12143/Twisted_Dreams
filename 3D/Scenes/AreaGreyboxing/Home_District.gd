e603516f48b24729dd1ca06a3f4afa47322e808a
Rookioo <146274825+Rookioo@users.noreply.github.com>
2024-07-04 12:10:03 -0700

    Revert "Scene Loading Changes"
    This reverts commit ea348bc27beabca1f8300d975a9bdbab2191168b.

---
+++ 3D/Scenes/AreaGreyboxing/Home_District.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var transition = $Transition_Manager_Home_District/Transition
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#transition.play("fade_in")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
