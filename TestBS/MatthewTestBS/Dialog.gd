5b213cf01faa6951929fdaf2c9e3c33da501521f
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-07-10 11:36:17 -0500

    Worked on more dialog

--- TestBS/MatthewTestBS/Dialog.gd
+++ TestBS/MatthewTestBS/Dialog.gd
@@ -5,24 +5,24 @@ extends Control
 var page: int = 0
 
 func _ready():
-	$DialogText.bbcode_text = dialog[page]
-	$DialogText.set_visible_characters(0)
-	$Timer.set_wait_time(wait_time)
-
-func _process(delta):
-	if $DialogText.get_visible_characters() > $DialogText.get_total_character_count():
-		if page < dialog.size() - 1:
-			await(get_tree().create_timer(0.5).timeout)
-			change_page()
-		elif page == dialog.size() - 1:
-			await(get_tree().create_timer(1).timeout)
-			hide()
+	if page < dialog.size():
+		$Polygon2D/DialogText.bbcode_text = dialog[page]
+		$Polygon2D/DialogText.set_visible_characters(0)
+		$Timer.set_wait_time(wait_time)
 
 func change_page():
-	page += 1
-	print(page)
-	$DialogText.bbcode_text = dialog[page]
-	$DialogText.set_visible_characters(0)
+	if page < dialog.size() - 1:
+		await(get_tree().create_timer(0.5).timeout)
+		page += 1
+		$Polygon2D/DialogText.bbcode_text = dialog[page]
+		$Polygon2D/DialogText.set_visible_characters(0)
+		$Timer.start()
+	elif page == dialog.size() - 1:
+		await(get_tree().create_timer(1).timeout)
+		hide()
 
 func _on_timer_timeout():
-	$DialogText.set_visible_characters($DialogText.get_visible_characters() + 1)
+	$Polygon2D/DialogText.set_visible_characters($Polygon2D/DialogText.get_visible_characters() + 1)
+	if $Polygon2D/DialogText.get_visible_characters() > $Polygon2D/DialogText.get_total_character_count():
+		$Timer.stop()
+		change_page()
