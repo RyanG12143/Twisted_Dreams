648d6a85f7621048ca3bbbd537d3154aeaabd19c
RyanG12143 <loop9981@gmail.com>
2024-07-12 19:33:04 -0500

    NPC nav fixes
    NPC desired distance does not care about height for easier marker placement. No longer gets stuck on small elevation changes

--- TestBS/RyanTestBS/Chase/Chase_Character.gd
+++ TestBS/RyanTestBS/Chase/Chase_Character.gd
@@ -4,19 +4,19 @@ extends CharacterBody3D
 ## Inverse Loop
 
 
-const SPEED = 5.0
-
+@export var speed = 5.0
+@export var turn_Speed:float = 10
 @export var loop:bool = false
 @export var positions_container:Node3D
-@export var turn_Speed:float = 10
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var currentPosition:int = 0
+var current_position:int = 0
 var positions:Array[Vector3]
 var child_look_at
 
-@onready var navAgent:NavigationAgent3D = $NavigationAgent3D
+@onready var nav_agent:NavigationAgent3D = $NavigationAgent3D
 @onready var sample:Node3D = $Node3D
 
 
@@ -29,8 +29,8 @@ func _ready():
 		if marker is Marker3D:
 			positions.append(marker.global_position)
 	
-	navAgent.target_position = positions[currentPosition]
-	currentPosition += 1
+	nav_agent.target_position = positions[current_position]
+	current_position += 1
 	
 	child_look_at = Marker3D.new()
 	add_child(child_look_at)
@@ -41,9 +41,9 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	navAgent.target_position = positions[currentPosition - 1]
+	nav_agent.target_position = positions[current_position - 1]
 	
-	var target = navAgent.get_next_path_position()
+	var target = nav_agent.get_next_path_position()
 	child_look_at.look_at(Vector3(target.x, child_look_at.global_position.y, target.z), Vector3.UP, true)
 	var target_rotation = Quaternion(child_look_at.global_transform.basis)
 	var current_rotation = Quaternion(global_transform.basis)
@@ -52,21 +52,30 @@ func _physics_process(delta):
 	
 	var direction = ($Front.global_position - global_position).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 
 
+func _process(delta):
+	var targ_pos = nav_agent.target_position
+	targ_pos.y = global_position.y
+	var dist_targ = global_position.distance_to(targ_pos)
+	
+	if dist_targ < nav_agent.target_desired_distance:
+		nav_agent.emit_signal("target_reached")
+
+
 func _on_target_reached():
 	print("Target")
-	if positions.size() > currentPosition:
-		navAgent.target_position = positions[currentPosition]
-		currentPosition += 1
+	if positions.size() > current_position:
+		nav_agent.target_position = positions[current_position]
+		current_position += 1
 	elif loop:
-		currentPosition = 0
-		navAgent.target_position = positions[currentPosition]
-		currentPosition += 1
+		current_position = 0
+		nav_agent.target_position = positions[current_position]
+		current_position += 1
