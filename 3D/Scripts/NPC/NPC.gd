cf3cbed074104b8ecee3c76e18039f260999e9d1
RyanG12143 <loop9981@gmail.com>
2024-07-14 17:05:02 -0500

    NPCs can now be given new paths when reaching waypoints

--- 3D/Scripts/NPC/NPC.gd
+++ 3D/Scripts/NPC/NPC.gd
@@ -3,40 +3,52 @@ extends CharacterBody3D
 # Cops look at passerby
 # Inverse Loop
 
+signal finished_path
 
+## Movement speed of NPC
 @export var speed = 5.0
+## Turn speed of NPC
 @export var turn_Speed:float = 10
+## If true NPC will travel in loop through each waypoint
 @export var loop:bool = false
+## If true npc will backtrack through waypoints. (1234321)[br]
+## MUST HAVE LOOP ENABLED
 @export var inverse_loop:bool = false
+## Node3d containing all waypoints for npc travel to
 @export var positions_container:Node3D
 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
+## Enabling changing of speed while allowing going back to default
+var speed_multiplyer:float = 1
+## Incremental number to keep track of curent desired waypoint
 var current_marker:int = 0
+## Used for inverse looping
 var traverse_direction:int = 1
+## Container for all marker destinations for npc
 var markers:Array[Marker3D]
-var child_look_at
+## Boolean for waiting at desired waypoint
 var waiting:bool = false
+## Target to turn towards when reaching waypoint
 var wait_turn_target:Vector3 = Vector3.ZERO
 
+## Navigation agent to provide pathfinding for npc
 @onready var nav_agent:NavigationAgent3D = $NavigationAgent3D
-@onready var sample:Node3D = $Node3D
+## Timer to set wait time
 @onready var timer:Timer = $Timer
+## Marker to enable correct desired rotation
+@onready var child_look_at:Marker3D = Marker3D.new()
 
 
 
 func _ready():
+	# Disable processing if not given path to follow (Easier to debug)
 	if not positions_container:
 		process_mode = Node.PROCESS_MODE_DISABLED
 		return
-	for marker in positions_container.get_children():
-		if marker is Marker3D:
-			markers.append(marker)
+	_fill_waypoints()
 	
-	nav_agent.target_position = markers[current_marker].global_position
-	
-	child_look_at = Marker3D.new()
 	add_child(child_look_at)
 
 
@@ -45,6 +57,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
+	# Waiting at waypoint
 	if waiting:
 		_turn(delta, wait_turn_target)
 		return
@@ -53,13 +66,14 @@ func _physics_process(delta):
 	
 	_turn(delta, nav_agent.get_next_path_position())
 	
+	# Character movement
 	var direction = ($Front.global_position - global_position).normalized()
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * speed * speed_multiplyer
+		velocity.z = direction.z * speed * speed_multiplyer
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplyer)
+		velocity.z = move_toward(velocity.z, 0, speed * speed_multiplyer)
 
 	move_and_slide()
 
@@ -69,6 +83,7 @@ func _process(delta):
 	targ_pos.y = global_position.y
 	var dist_targ = global_position.distance_to(targ_pos)
 	
+	# Checking if reached target without height
 	if dist_targ < nav_agent.target_desired_distance:
 		nav_agent.emit_signal("target_reached")
 
@@ -77,6 +92,7 @@ func _on_target_reached():
 	
 	var marker = markers[current_marker]
 	
+	# Chekcing and assigning waypoint values
 	if marker.get_script() != null:
 		if marker.wait:
 			waiting = true
@@ -86,14 +102,24 @@ func _on_target_reached():
 			if marker.speed_value > 0:
 				speed = marker.speed_value
 			if marker.speed_multiplyer > 0:
-				speed *= marker.speed_multiplyer
+				speed_multiplyer = marker.speed_multiplyer
 		
 		if marker.turn_towards:
 			wait_turn_target = marker.turn_target.global_position
 		else:
 			wait_turn_target = global_position
+		
+		if marker.give_new_path:
+			if not marker.random_new_path:
+				positions_container = marker.new_paths[0]
+			else:
+				var new_path = randi_range(0, marker.new_paths.size() - 1)
+				positions_container = marker.new_paths[new_path]
+			
+			_fill_waypoints()
+			return
 	
-	
+	# Deciding to loop a
 	if markers.size() > current_marker + 1 and not (current_marker + (1 * traverse_direction)) < 0:
 		current_marker += 1 * traverse_direction
 		nav_agent.target_position = markers[current_marker].global_position
@@ -109,15 +135,30 @@ func _on_target_reached():
 		current_marker = 0
 		nav_agent.target_position = markers[current_marker].global_position
 		return
+	
+	
+	emit_signal("finished_path")
 
 
 func _on_wait_timeout():
 	waiting = false
 
 
+## Turns the chracter towards target
 func _turn(delta, target:Vector3):
 	child_look_at.look_at(Vector3(target.x, child_look_at.global_position.y, target.z), Vector3.UP, true)
 	var target_rotation = Quaternion(child_look_at.global_transform.basis)
 	var current_rotation = Quaternion(global_transform.basis)
 	var next_rotation = current_rotation.slerp(target_rotation, delta * turn_Speed)
 	global_transform.basis = Basis(next_rotation)
+
+
+## Refills markers from positions_container while setting current_marker to 0
+func _fill_waypoints():
+	current_marker = 0
+	markers.clear()
+	for marker in positions_container.get_children():
+		if marker is Marker3D:
+			markers.append(marker)
+	
+	nav_agent.target_position = markers[current_marker].global_position
--- 3D/Scripts/NPC/Waypoint.gd
+++ 3D/Scripts/NPC/Waypoint.gd
@@ -1,10 +1,23 @@
 extends Marker3D
 
+## NPC will wait at waypoint when enabled
 @export var wait:bool = false
+## Time that npc will wait at waypoint
 @export var wait_time:float = 1
+## NPC will change speed to given values if values are > 0
 @export var change_speed:bool = false
+## NPC will set speed to given value
 @export var speed_value:float = 0
+## NPC will set speed multiplyer to given value
 @export var speed_multiplyer:float = 0
+## NPC will turn towards Marker3D Child when enabled
 @export var turn_towards:bool = false
+## NPC will be given a new path from New Paths when enabled
+@export var give_new_path:bool = false
+## Will make path selection random
+@export var random_new_path:bool = false
+## Paths npc will choose from when being given new path
+@export var new_paths:Array[Node3D]
 
+## Target to turn towards when turn_towards is enabled
 @onready var turn_target:Marker3D = $Marker3D
