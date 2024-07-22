5515f7cee2fd36e1c0962a1dc57416c76ff57ebd
Brodbeatz <brodbeckm8002@my.uwstout.edu>
2024-07-22 11:59:24 -0500

    Updated dialog system

--- TestBS/LeoPuzzlePrototype/Trigger_Box.gd
+++ TestBS/LeoPuzzlePrototype/Trigger_Box.gd
@@ -6,13 +6,14 @@ extends Area2D
 ##Is this trigger box for bringing up a dialog box
 @export var dialog_trigger:bool = false
 ##Dialog to send to the dialog box
-@export var char_1_dialog: Array[String] = []
-@export var char_2_dialog: Array[String] = []
-##The starting dialog 1 = character 1, 2 = character 2, 0 = only one person speaking
-@export var starting: int = 0
+@export var dialog: Array[Node] = []
+#@export var char_1_dialog: Array[String] = []
+#@export var char_2_dialog: Array[String] = []
+###The starting dialog 1 = character 1, 2 = character 2, 0 = only one person speaking
+#@export var starting: int = 0
 
 signal Trigger_Activated
-signal Send_Dialog(char_1, char_2, start)
+signal Send_Dialog(dialogue)
 
 ## Whether or not the trigger is activated.
 var trigger_activated:bool = false
@@ -24,6 +25,6 @@ func _trigger_dectected_body(body):
 		if(follow_trigger):
 			globals.toggle_follow_state()
 		elif(dialog_trigger):
-			emit_signal("Send_Dialog", char_1_dialog, char_2_dialog, starting)
+			emit_signal("Send_Dialog", dialog)
 		else:
 			emit_signal("Trigger_Activated")
