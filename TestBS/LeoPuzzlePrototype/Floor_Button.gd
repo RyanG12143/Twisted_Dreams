bdd6b645776267e10170d6193ad8ce426653752c
Leo Dresang <lrdresang@yahoo.com>
2024-07-23 18:39:52 -0500

    floor buttons implementation

--- TestBS/LeoPuzzlePrototype/Floor_Button.gd
+++ TestBS/LeoPuzzlePrototype/Floor_Button.gd
@@ -1,6 +1,12 @@
-extends AnimatedSprite2D
+extends ColorCodedMechanic
 class_name Floor_Button
 ## Button with two states, pressed or released.
+
+@onready var anim:AnimationPlayer = $AnimationPlayer
+@onready var audio:AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+@export var button_press_sound:AudioStreamWAV = null
+@export var button_release_sound:AudioStreamWAV = null
 
 signal Button_Pressed
 signal Button_Released
@@ -12,18 +18,24 @@ var overlapping_bodies:Array = []
 
 ## Sets default frame.
 func _ready():
-	frame = 0
+	if(color == "blue"):
+		animation = "Blue"
+	elif(color == "pink"):
+		animation = "Pink"
+	elif(color == "orange"):
+		animation = "Orange"
+	set_frame(0)
 
 ## Handles bodies pressing the button.
 func _on_area_2d_body_entered(body):
 	if(body.is_in_group("Can_Press_Buttons")):
 		if(!overlapping_bodies):
 			emit_signal("Button_Pressed")
+			var temp = get_frame()
+			anim.stop()
+			anim.play("Press")
+			set_frame(temp)
 		overlapping_bodies.append(body)
-		recent_pressed = true
-		while (frame != 3 and recent_pressed == true):
-			frame += 1
-			await get_tree().create_timer(0.03).timeout
 
 ## Handles bodies being removed from the button.
 func _on_area_2d_body_exited(body):
@@ -31,7 +43,16 @@ func _on_area_2d_body_exited(body):
 		overlapping_bodies.erase(body)
 		if(!overlapping_bodies):
 			emit_signal("Button_Released")
-			recent_pressed = false
-			while (frame != 0 and recent_pressed == false):
-				frame -= 1
-				await get_tree().create_timer(0.03).timeout
+			var temp = get_frame()
+			anim.stop()
+			anim.play("Release")
+			set_frame(temp)
+
+func play_sound():
+	if (!overlapping_bodies):
+		audio.stream = button_release_sound
+		audio.volume_db = 0
+	else:
+		audio.stream = button_press_sound
+		audio.volume_db = -3
+	audio.play()
