cd00c30ef9fc45b12a98b791202798d7e5768dd1
Rookioo <andersond5235@my.uwstout.edu>
2024-08-04 16:53:20 -0500

    Chase scene done

--- 3D/Scripts/Level_Design/Breaking_Glass_Bridge.gd
+++ 3D/Scripts/Level_Design/Breaking_Glass_Bridge.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var player: CharacterBody3D
 @export var collider: Area3D
+@export var glass_collider: CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +10,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if collider.overlaps_body(player):
-		pass
-		#print("collided")
-		#queue_free()
+	if collider.overlaps_body(player) && glass_collider != null:
+		glass_collider.queue_free()
+		queue_free()
---
+++ 3D/Scripts/Level_Design/Chase_Trigger_Bridge_Officers.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var player: CharacterBody3D
+@export var collider: Area3D
+@export var chasing_officers: Node3D
+var event_triggered: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if collider.overlaps_body(player) && !event_triggered:
+		for chasing_officer in chasing_officers.get_children():
+			chasing_officer.chasing = true
+		event_triggered = true
