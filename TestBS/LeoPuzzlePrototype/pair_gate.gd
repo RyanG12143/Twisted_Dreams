61d0653909fb5fc715e70fdb8eacd50d02cad7e1
Leo Dresang <lrdresang@yahoo.com>
2024-08-14 22:34:25 -0500

    pair gates visuals (bugged rn, need to fix)

--- TestBS/LeoPuzzlePrototype/pair_gate.gd
+++ TestBS/LeoPuzzlePrototype/pair_gate.gd
@@ -2,9 +2,11 @@ extends Node
 ## A gate that opens when both characters are near it, and blocks the player from going backwards.
 
 ## Sprites and colliders for the gate and blocker, respectively.
-@onready var gate_sprite:Sprite2D = $Gate/Sprite2D
+@onready var gate_pillar:AnimatedSprite2D = $Gate/ForwardPillar
+@onready var gate_bars:AnimatedSprite2D = $Gate/Gate
 @onready var gate_collider:CollisionShape2D = $Gate/CollisionShape2D
-@onready var blocker_sprite:Sprite2D = $Path_Blocker/Sprite2D
+@onready var blocker_pillar:AnimatedSprite2D = $Path_Blocker/ForwardPillar
+@onready var blocker_bars:AnimatedSprite2D = $Path_Blocker/Gate
 @onready var blocker_collider:CollisionShape2D = $Path_Blocker/CollisionShape2D
 
 ## Number of characters within the detection area.
@@ -15,17 +17,19 @@ var gate_used:bool = false
 
 ## Disables and hides the blocker.
 func _ready():
-	blocker_sprite.visible = false
+	gate_pillar.set_frame(0)
+	gate_bars.set_frame(0)
+	blocker_pillar.set_frame(0)
+	blocker_bars.set_frame(11)
 	blocker_collider.set_deferred("disabled", true)
 
 ## Enables and reveals the blocker.
 func enable_blocker():
-	blocker_sprite.visible = true
 	blocker_collider.set_deferred("disabled", false)
+	
 
 ## Disables and hides the gate.
 func disable_gate():
-	gate_sprite.visible = false
 	gate_collider.set_deferred("disabled", true)
 
 
@@ -33,6 +37,10 @@ func _on_detection_area_body_entered(body):
 	if(!gate_used):
 		if body.is_in_group("Player") and (nearby_character_num < 2):
 			nearby_character_num += 1
+			if (body == globals.character_one):
+				gate_pillar.set_frame(3)
+			else:
+				gate_pillar.set_frame(2)
 			if(nearby_character_num == 2):
 				enable_blocker()
 				disable_gate()
@@ -42,3 +50,4 @@ func _on_detection_area_body_exited(body):
 	if(!gate_used):
 		if body.is_in_group("Player") and (nearby_character_num > 0) :
 			nearby_character_num -= 1.
+			gate_pillar.set_frame(0)
